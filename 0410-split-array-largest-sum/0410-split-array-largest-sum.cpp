082aa244fc90adec249845685e115293def74eb9
Riya Gupta <139098932+Riya922003@users.noreply.github.com>
2024-12-02 13:26:11 +0530

    Time: 0 ms (100.00%), Space: 10.3 MB (8.67%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+      bool helper(vector<int>& nums, int mid, int d) {
+        int count = 1, sum = 0;  // Start with 1 day
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            if (sum > mid) {  // Exceeds the capacity for the current group
+                count++;
+                sum = nums[i];  // Start a new group
+            }
+        }
+        return count <= d;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        int n=nums.size();
+        int left = *max_element(nums.begin(), nums.end());
+        int right = accumulate(nums.begin(), nums.end(), 0);
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (helper(nums, mid, k)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
